049d0de5f3ade49fedacae367965cde3c400ad32
Ighus <igor.ffp@gmail.com>
2017-05-23 22:37:51 -0300

    Add files via upload

---
+++ UTI_GrupoLaranja.DAL/Conexao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace UTI_GrupoLaranja.DAL
+{
+    public class Conexao
+    {
+        public static string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MinhaConexao"].ConnectionString;
+        public static SqlConnection connection = new SqlConnection(connectionString);
+
+        public static void conectar()
+        {
+
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+        }
+
+        public static void desconectar()
+        {
+
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ UTI_GrupoLaranja.Entities/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTI_GrupoLaranja.Entities
+{
+    class Paciente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public DateTime idade { get; set; }
+        public char sexo { get; set; }
+        public double temperatura { get; set; }
+        public double pressao { get; set; }
+
+    }
+}
